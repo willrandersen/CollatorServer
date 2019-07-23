8c70dff7bb0847bee7997794ae51c2fb4e2f3d91
wra3@illinois.edu <Locomotives12moby>
2019-07-23 11:16:47 -0500

    Removed attempted speed boost, made queue more efficient

--- flask_runner.py
+++ flask_runner.py
@@ -516,7 +516,7 @@ def send_loaded_file(task_id):
 @app.route('/Login-Data', methods=['POST'])
 def handle_login():
     session = requests.Session()
-    session.mount('https://', adapters.HTTPAdapter(pool_connections=24, pool_maxsize=24))
+    #session.mount('https://', adapters.HTTPAdapter(pool_connections=24, pool_maxsize=24))
     username = request.form['Username']
     password = request.form['Password']
 
