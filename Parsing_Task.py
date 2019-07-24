b025cfe1bfcb486f2221cbd0ef6e6dbe795da060
wra3@illinois.edu <Locomotives12moby>
2019-07-24 11:35:11 -0500

    Changed status thread count

--- Parsing_Task.py
+++ Parsing_Task.py
@@ -13,7 +13,7 @@ from Task_Queue import Task, full_run
 from multithreaded_status import *
 
 SHIPPING_THREAD_MAX = 24
-STATUS_REQUEST_THREADS = 8
+STATUS_REQUEST_THREADS = 24
 
 def merge_MOL_DS(DS_table, MOL_table, MOL_header):
     MOL_header.append('Internal Comments (DS Items)')
